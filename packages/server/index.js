e261fd369b613fe6ac136f540c7739d17d34345b
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-09 13:21:15 -0400

    removed alias and cors

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,15 +1,14 @@
 import 'dotenv/config'
 import express from 'express'
-import cors from 'cors'
+// import cors from 'cors'
 import morgan from 'morgan'
-import hello from '#controllers/hello'
+import hello from './src/controllers/hello'
 
-let PORT = 5000
 const app = express()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 app.use(morgan('dev'))
 
 // serving static files
@@ -19,7 +18,7 @@ app.use(express.static('../client/dist'))
 app.use('/api/hello', hello)
 
 // start server
-PORT = process.env.PORT || PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 export default app
